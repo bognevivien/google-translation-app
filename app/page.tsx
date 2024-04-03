78f954a77e1fecc103759b75202369b88fa122f0
bognevivien <bognetienoue@gmail.com>
2024-04-03 17:51:16 +0200

    fix: update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,14 @@
-import TranslationForm from "@/components/TranslationForm";
-import { auth } from "@clerk/nextjs/server";
-import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
-export type TranslationLanguages = {
-  translation: {
-    [key: string]: {
-      name: string;
-      nativeName: string;
-      dir: "ltr" | "rtl";
-    };
-  };
-};
-
-async function Home() {
-  auth().protect();
-
-  const { userId } = auth();
-  if (!userId) throw new Error("User not logged in");
-
-  const languagesEndpoint =
-    "https://api.cognitive.microsofttranslator.com/languages?api-version=3.0";
-
-  const response = await fetch(languagesEndpoint, {
-    next: {
-      revalidate: 60 * 60 * 24, // cache the results for 24 hours and then refresh
-    },
-  });
-
-  const languages = (await response.json()) as TranslationLanguages;
+export default function Home() {
   return (
-    <div>
-      <TranslationForm languages={languages} />
-    </div>
+    <main className="">
+      <Link href={`/translate`}>
+        {" "}
+        <Button>Translation Here</Button>
+      </Link>
+    </main>
   );
 }
-
-export default Home;
